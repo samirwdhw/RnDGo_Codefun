07a4f35529354170e1ec0c279d34e1af29c26991
Samir <samir.wdhw@gmail.com>
2016-11-21 21:13:57 +0530

    Basic Login Made

--- RndGo/urls.py
+++ RndGo/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""RndGo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views
+from log.forms import LoginForm
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('log.urls')),
+    url(r'^login/$', views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name='login'),
+    url(r'^logout/$', views.logout, {'next_page': '/login'}),
 ]
